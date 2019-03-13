e89c67b897c08985b0d099ddd90f22c9874b6e48
Nicholas Devenish <ndevenish@gmail.com>
2019-03-13 12:09:08 +0000

    Use six for StringIO

--- src/pytest_libtbx/fake_env.py
+++ src/pytest_libtbx/fake_env.py
@@ -1,5 +1,7 @@
+# coding: utf-8
+from __future__ import absolute_import, division, print_function
 import sys
-from StringIO import StringIO
+from six.moves import StringIO
 
 
 class CustomRuntestsEnvironment:
